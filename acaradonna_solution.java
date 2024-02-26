9fd0cc7e7810e352c2c13a1aae00d223723d3f03
Alex Caradonna (alpackle) <54595366+acaradonna@users.noreply.github.com>
2024-02-26 15:20:03 -0500

    Create acaradonna_solution.java

---
+++ acaradonna_solution.java
@@ -0,0 +1,55 @@
+public class IslandCount {
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int numIslands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0'; // Mark the current cell as visited
+        // Explore adjacent cells
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        IslandCount islandCount = new IslandCount();
+
+        char[][] grid1 = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
+        System.out.println(islandCount.numIslands(grid1)); // Output: 1
+
+        char[][] grid2 = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+        System.out.println(islandCount.numIslands(grid2)); // Output: 3
+    }
+}
